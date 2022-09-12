46f1600e1107d2b24f834106eebdeb13489d7a30
mellanahus <Mellanahus02SJ@gmail.com>
2022-09-12 17:54:15 +0700

    Percobaan menggunakan fungsi dan lambdas

---
+++ src/fungsiDanLambdas/fungsi01.kt
@@ -0,0 +1,14 @@
+package fungsiDanLambdas
+
+// Penggunaan fungsi tanpa nilai kembali
+
+fun main(args: Array<String>) {
+    displayMessage("Rekayasa Perangkat Lunak", 10);
+}
+fun displayMessage(msg: String, count: Int) {
+    var counter = 1
+    while(counter <= count ) {
+        println("$msg $counter")
+        counter++
+    }
+}
